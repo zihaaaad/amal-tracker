10fa70c835a1050f8267e8ece5d378d7d64ee9a2
Zihad Hasan <zihad.connects@gmail.com>
2026-05-13 14:06:49 +0600

    ci: use AGP 8.1.0 + Gradle 8.3 for isar_flutter_libs namespace compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,23 +19,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// Fix for old plugins (like isar_flutter_libs) that don't declare a namespace.
-// AGP 8.x requires all library modules to have a namespace.
-subprojects {
-    afterEvaluate {
-        if (project.plugins.hasPlugin("com.android.library")) {
-            val androidExt = project.extensions.findByName("android")
-            if (androidExt is com.android.build.gradle.LibraryExtension) {
-                if (androidExt.namespace.isNullOrEmpty()) {
-                    androidExt.namespace = project.group.toString().ifEmpty {
-                        "dev.flutter.${project.name.replace("-", "_")}"
-                    }
-                }
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
